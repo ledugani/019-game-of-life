0b219d4fa89b5703d14d3b613a2f9bac950eddb5
Tom Dugan <thomas@aadwebb.com>
2020-01-13 14:28:33 -0600

    working on vector mat

--- src/physics/specimen.js
+++ src/physics/specimen.js
@@ -9,6 +9,31 @@ class Specimen extends Entity {
     props.maxSpeed = Specimen.maxSpeed
     super(props)
   }
+
+  seek(target) {
+    if (!target) return
+    this.moveTowards(target)
+  }
+
+  moveTowards(target) {
+    const force = this.steer(target)
+  }
+
+  steer(target) {
+    const desiredForce = target.position.subtract(this.position)
+    const distance = desiredForce.length()
+    const variableForce = (distance < 10)
+      ? this.maxForce * (distance / 100)
+      : this.maxForce
+
+    return desiredForce.normalize()
+                        .multiply(variableForce)
+                        .subtract(this.velocity)
+  }
+
+  update() {
+    this.position = this.position.add(this.velocity)
+  }
 }
 
 export default Specimen;
